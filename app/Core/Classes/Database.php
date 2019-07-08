9ff7a4ef2facbe89b12b0efa5052c8a2b2c62689
Sanders Smith Gutiérrez Mena <ing.sanders@gmail.com>
2019-07-08 08:21:13 -0300

    Acceso a datos con Doctrine DBAL
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ app/Core/Classes/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Core\Classes;
+
+use App\Core\Traits\Singleton;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+
+/**
+ * Gestiona la conexión a la base de datos
+ */
+class Database
+{
+    /**
+     * Provee la funcionalidad para el uso del Patŕon Singleton
+     */
+    use Singleton;
+
+    /**
+     * Se conecta a la base de datos
+     * 
+     * @return object \Doctrine\DBAL\DriverManager
+     */
+    public function connect()
+    {
+        $config = new Configuration();
+
+        $connectionParams = array(
+            'dbname' => 'eduvota',
+            'user' => 'sanders',
+            'password' => '12345',
+            'host' => 'localhost',
+            'driver' => 'mysqli',
+        );
+
+        return DriverManager::getConnection($connectionParams, $config);
+    }
+}
