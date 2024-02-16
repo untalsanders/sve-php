4212e546db8ab8ce253adaee839c2fc5622cd455
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-02-16 05:54:03 -0300

    chore: layout admin is ok
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- apps/Controllers/AdminController.php
+++ apps/Controllers/AdminController.php
@@ -14,6 +14,16 @@ class AdminController extends AbstractController
      */
     public function home($request, $response, array $args)
     {
-        return $this->view($response, "admin/home.twig", []);
+        $sql = "SELECT t1.username, t1.email, t2.role_name, t3.meta_key, t3.meta_value
+                FROM users t1
+                LEFT JOIN roles t2 ON t1.role_id = t2.id
+                LEFT JOIN meta_user t3 ON t1.id = t3.user_id
+                WHERE username = ? AND password = ?";
+
+        $username = $request->getParsedBody()['username'];
+        $password = $request->getParsedBody()['password'];
+        $user = $this->db()->fetchAssoc($sql, [$username, md5($password)]);
+
+        return $this->view($response, 'admin/home.twig', ['user' => $user]);
     }
 }
